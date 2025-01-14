4990629df0b436f404fff58ff90eb62dc44ee52d
Lucaspastana <lucasamorimpast@gmail.com>
2025-01-14 11:32:04 -0300

    returns2

--- aula 10 (functions)/returns.js
+++ aula 10 (functions)/returns.js
@@ -1,9 +1,26 @@
-function falaFrase(comeco){
+// TIPO DE FUNÇOES PARECIDO COMO BONECAS RUSSAS, FICA UMA NO ESCOPO DA OUTRA.
+
+/*function falaFrase(comeco){
     function falaResto(resto){
         return comeco + ' ' + resto;
     }
     return falaResto;
 }
 
-const olamundo = falaFrase('ola')
-console.log(olamundo('mundo'))
+const fala = falaFrase('ola')
+const resto = fala('mundo');
+console.log(resto); */
+
+function criaMultiplicador(multiplicador){
+    return function (n){
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+const quadriplica = criaMultiplicador(4)
+
+console.log(duplica(2))
+console.log(triplica(3))
+console.log(quadriplica(4))
