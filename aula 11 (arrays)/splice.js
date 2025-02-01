44684d2a65e93b64c283e9f36c22e5265a0d56ef
Lucaspastana <lucasamorimpast@gmail.com>
2025-02-01 00:43:53 -0300

    arrays2

--- aula 11 (arrays)/splice.js
+++ aula 11 (arrays)/splice.js
@@ -3,8 +3,17 @@
 const nomes = ['maria','joao','eduardo','gabriel','juliana'];
 //nomes.splice(indice, delete, elem1, elem2, elem3);
 
-                               //AQUI COMEÇOU 
-const removidos = nomes.splice(-2,Number.MAX_VALUE);
-console.log(nomes,removidos);
+//FUNÇAO DE UNSHIFT, A PARTIR DO INDICE 2 ELE VAI ADICIONAR OS NOMES
+nomes.splice(2,0,'lucas','amorim');                               
+
+
+
+//AQUI COMEÇOU 
+//const removidos = nomes.splice(-2,Number.MAX_VALUE);
+
+console.log(nomes);
 
 //The splice() method of Array instances changes the contents of an array by removing or replacing existing elements and/or adding new elements
+
+//CONCATENANDO
+
